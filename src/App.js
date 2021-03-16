9f40a93ba7babb9c9c4d21ab3493b215086440d7
jonathanmtran <jonathanmtran@users.noreply.github.com>
2021-03-15 21:55:57 -0700

    refactor: refactored App.js to conditionally show components when selected

--- src/App.js
+++ src/App.js
@@ -1,18 +1,74 @@
+import React from 'react';
+
 import './App.css';
+
 import CatFacts from './CatFacts.js';
 import Posts from './Posts.js';
 import Welcome from './Welcome.js';
-import Yo from './Yo.js';
 import Woof from './Woof.js';
+import Yo from './Yo.js';
 
-function App() {
-  return (
-    <div className="App">
-      <CatFacts count="3" />
-      <hr />
-      <Woof />
-    </div>
-  );
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showCatFacts: false,
+      showWoof: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    let value = event.target.value;
+
+    let defaultState = {
+      showCatFacts: false,
+      showWoof: false,
+    };
+    let newState = {};
+
+    if (value === 'CatFacts') {
+      newState = { showCatFacts: true };
+    }
+    else if (value === 'Woof') {
+      newState = { showWoof: true };
+    }
+
+    this.setState({...defaultState, ...newState});
+  }
+
+  render() {
+    let catFactsComponent = this.state.showCatFacts ? <CatFacts count="3" /> : undefined;
+    let woofComponent = this.state.showWoof ? <Woof /> : undefined;
+
+    return (
+      <div className="App">
+        <Container>
+          <h1>my-app</h1>
+          <Form>
+            <Form.Group>
+              <Form.Label>Component</Form.Label>
+              <Form.Control as="select" onChange={this.handleChange}>
+              <option>None</option>
+                <option value="CatFacts">CatFacts</option>
+                <option value="Woof">Woof</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </Container>
+        <Container>
+        { catFactsComponent }
+        { woofComponent }
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
